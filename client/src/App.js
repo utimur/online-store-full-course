95196b3711a5fb7b04deca98a4573866e1a42923
Mark <markukr7@gmale.com>
2022-11-07 20:50:56 +0100

    add new directory _course

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
-import React, {useContext, useEffect, useState} from 'react';
-import {BrowserRouter} from "react-router-dom";
+import React, { useContext, useEffect, useState } from 'react';
+import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./components/AppRouter";
 import NavBar from "./components/NavBar";
-import {observer} from "mobx-react-lite";
-import {Context} from "./index";
-import {check} from "./http/userAPI";
-import {Spinner} from "react-bootstrap";
+import { observer } from "mobx-react-lite";
+import { Context } from "./index";
+import { check } from "./http/userAPI";
+import { Spinner } from "react-bootstrap";
 
 const App = observer(() => {
-    const {user} = useContext(Context)
+    const { user } = useContext(Context)
     const [loading, setLoading] = useState(true)
 
     useEffect(() => {
@@ -19,7 +19,7 @@ const App = observer(() => {
     }, [])
 
     if (loading) {
-        return <Spinner animation={"grow"}/>
+        return <Spinner animation={"grow"} />
     }
 
     return (
--- client/src/store/UserStore.js
+++ client/src/store/UserStore.js
@@ -1,4 +1,4 @@
-import {makeAutoObservable} from "mobx";
+import { makeAutoObservable } from "mobx";
 
 export default class UserStore {
     constructor() {
